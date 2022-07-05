56ccde72df3b89788a66fffbcb755207c5a645fe
GiuseppeTumminello <Giuseppe.Tumminello@acoustic.co>
2022-07-05 16:53:52 +0200

    modified GenerationType from Sequence to Indentity

--- src/main/java/com/acoustic/entity/AnnualNet.java
+++ src/main/java/com/acoustic/entity/AnnualNet.java
@@ -18,7 +18,7 @@ import java.math.BigDecimal;
 @Builder
 public class AnnualNet {
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
     private BigDecimal annualNetAmount;
